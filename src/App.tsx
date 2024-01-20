cdc0d22b4d8cca7ecfd1bce46019dd64f133a710
AugustDespair <ethanng0802@gmail.com>
2024-01-21 01:03:36 +0800

    Update read functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import CreateGame from './Components/CreateGame';
 import JoinGame from './Components/JoinGame';
 import WaitingRoom from './Components/WaitingRoom';
 import WritePage from './Components/WritePage.tsx'; 
+import ReadPage from './Components/ReadPage.tsx'; 
 
 const App = () => {
  return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/join-game/:gameId" element={<JoinGame />} />
           <Route path='/waiting-room/:gameId' element={<WaitingRoom />} />
           <Route path='/game-room/:gameId' element={<WritePage />} />
+          <Route path='/game-room-read/:gameId' element={<ReadPage />} /> 
        </Routes>
     </>
  );
--- src/Components/ReadPage.tsx
+++ src/Components/ReadPage.tsx
@@ -1,19 +1,68 @@
 import { useState } from "react";
-import { doc, getDoc, setDoc, arrayUnion, onSnapshot } from "firebase/firestore";
-import { db } from "../firebase";
 
 const ReadPage = () => {
-
-    const getPlayerInputFromServer = () => {
+/*
+    const getPlayerInputFromServer = async() => {
         
         const currPlayerId = localStorage.getItem("playerName"); 
+        const currGameId = localStorage.getItem("gameId"); 
     }; 
+  */ 
     
+    const jumbleAndRandom = (s: string) => {
+        
+        const n = s.length; 
+
+        const s_arr = s.split("");
+
+        const numToAlter = Math.floor(n / 3) === 0 ? 1 : Math.floor(n / 3); 
+    
+        const alteredIndices = {}; 
+
+        for (let j = 0; j < Math.floor(numToAlter/2);  ++j) {
+            
+            let indexToRemove = undefined; 
+            let indexToAlter = undefined; 
+
+            while (alteredIndices[indexToRemove] !== undefined) {
+                indexToRemove = Math.floor(Math.random() * (n+1)); 
+                alteredIndices[indexToRemove] = 1;
+                s_arr[indexToRemove] = '_';
+            }
+
+            while (alteredIndices[indexToAlter] !== undefined) {
+                indexToAlter = Math.floor(Math.random() * (n+1)); 
+                alteredIndices[indexToAlter] = 1;
+                s_arr[indexToAlter] = String.fromCharCode(Math.floor(Math.random() * 26));
+            }
+        }
+
+        return s_arr.join(); 
+
+    }
+
+    // NOTE: Change this to generatePrompt()
+    const [prompt] = useState(""); 
+    const [playerPrompt, setPlayerPrompt] = useState("");
+
+    const generatePrompt = () => {
+    }; 
+
+
     return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h1 className="text-xl font-bold mb-4">Enter Your Prompt!</h1>
+        <h1 className="text-xl font-bold mb-4">Your prompt is</h1>
+        
+        <a className="text-xl font-bold mb-4" >{prompt}</a>
 
+        <input
+          id="userInput"
+          type="text"
+          value={playerPrompt}
+          onChange={(e) => setPlayerPrompt(e.target.value)}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
 
 
       </div>
